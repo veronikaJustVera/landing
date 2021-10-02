aa389b62fe56be7e75f7337bbf31975e0d79fd6c
Veronika <veronikajustvera@gmail.com>
2021-10-02 16:01:44 +0300

    add scroll-top-button

--- js/script.js
+++ js/script.js
@@ -59,3 +59,29 @@ function generate_donuts() {
 		donutLoop++;
     }
 }
+// scroll top
+(function() {
+    function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+  
+      if (scrolled > coords) {
+        goTopBtn.classList.add('top-button-show');
+      }
+      if (scrolled < coords) {
+        goTopBtn.classList.remove('top-button-show');
+      }
+    }
+  
+    function backToTop() {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+      }
+    }
+  
+    var goTopBtn = document.querySelector('.top-button');
+  
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+  })();
