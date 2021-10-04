a459d8f7ac123411036a9749628c66810f04981d
Veronika <veronikajustvera@gmail.com>
2021-10-04 20:15:12 +0300

    benefits block fixes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 window.onload = function() {
     // SLIDER
-    if (window.matchMedia("(max-width: 1078px)").matches) {
-        $('.benefit-block').slick(
-        {
-            infinite: false,
-            dots: true,
-            variableWidth: true,
-            speed: 300,
-            slidesToShow: 1,
-            customPaging: function(slider, i) {
-              return '<span class="custom-dot"></span>';
-            },
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            prevArrow: '<span class="slickArrow slickArrow--prev"></span>',
-            nextArrow: '<span class="slickArrow slickArrow--next"></span>',
-          });
+    if (window.matchMedia("(max-width: 900px)").matches) {
+      benefits_slider();
     }
 };
+$(window).resize(function(e){
+  if(window.innerWidth < 900) {
+      if(!$('.benefit-block').hasClass('slick-initialized')){
+        benefits_slider();
+      }
+
+  } else {
+      if($('.slider').hasClass('slick-initialized')){
+          $('.slider').slick('unslick');
+      }
+  }
+});
 // scroll top
 (function() {
     function trackScroll() {
@@ -44,3 +42,21 @@ window.onload = function() {
     window.addEventListener('scroll', trackScroll);
     goTopBtn.addEventListener('click', backToTop);
   })();
+function benefits_slider()
+{
+  $('.benefit-block').slick(
+    {
+        infinite: false,
+        dots: true,
+        variableWidth: true,
+        speed: 300,
+        slidesToShow: 1,
+        customPaging: function(slider, i) {
+          return '<span class="custom-dot"></span>';
+        },
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<span class="slickArrow slickArrow--prev"></span>',
+        nextArrow: '<span class="slickArrow slickArrow--next"></span>',
+    });
+}
